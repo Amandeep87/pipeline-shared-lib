15fe710376f332e1d0f112c999f8714e908602fe
Amandeep87 <55234970+Amandeep87@users.noreply.github.com>
2020-01-12 12:22:07 +0530

    Update execMaven.groovy

--- vars/execMaven.groovy
+++ vars/execMaven.groovy
@@ -1,3 +1,3 @@
-def call(String options = '-v') {
-    sh "mvn ${options}"
+def call(String options = '-v') { //define a function called call. string input paramter has a default value
+    sh "mvn ${options}" 
 }
