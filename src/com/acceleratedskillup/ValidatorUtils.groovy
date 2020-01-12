370b7175b6b5eafb110e20248db4870407a5ba03
Amandeep87 <55234970+Amandeep87@users.noreply.github.com>
2020-01-12 11:57:34 +0530

    Update ValidatorUtils.groovy

--- src/com/acceleratedskillup/ValidatorUtils.groovy
+++ src/com/acceleratedskillup/ValidatorUtils.groovy
@@ -2,7 +2,7 @@ package com.acceleratedskillup;
 
 class ValidatorUtils {
 
-	static def validateInput(script) {
+	static def validateInput(script) { //accepts a single parameter called pipeline script object. static method
     	script.echo "in static validateInput()"
   	}
 
