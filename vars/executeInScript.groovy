1fb97404e44dc8c68117565c6df0511acfa76f9a
Amandeep87 <55234970+Amandeep87@users.noreply.github.com>
2020-01-12 12:38:25 +0530

    Update executeInScript.groovy

--- vars/executeInScript.groovy
+++ vars/executeInScript.groovy
@@ -1,5 +1,5 @@
-def call(Closure body) {
-	script{
+def call(Closure body) { //define a call method which accepts a closure paramter. this is an anonymous block of code 
+	script{ //executes the block of code.
       body()
 	}    
 }
